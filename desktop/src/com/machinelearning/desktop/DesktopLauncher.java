461dd156a4a2cd9b80dc429856afb83bd1b2977b
cjoh88 <cjoh193@gmail.com>
2016-04-18 09:45:45 +0200

    first commit

---
+++ desktop/src/com/machinelearning/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.machinelearning.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.machinelearning.Evolution;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Evolution(), config);
+	}
+}
