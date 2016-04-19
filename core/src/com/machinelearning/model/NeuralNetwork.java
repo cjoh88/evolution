6bc61eed3eac7b0b962d6667908415cfd3857366
cjoh88 <cjoh193@gmail.com>
2016-04-19 12:27:41 +0200

    Started on a dumb neural network

--- core/src/com/machinelearning/model/NeuralNetwork.java
+++ core/src/com/machinelearning/model/NeuralNetwork.java
@@ -1,5 +1,20 @@
 package com.machinelearning.model;
 
+import java.util.Random;
+
 public class NeuralNetwork {
+	
+	private float weights[][];
+	private static Random random = new Random();
+	
+	public NeuralNetwork(int numInputs, int numHidden, int numOutputs) {
+		weights = new float[numInputs][numOutputs];
+		for(int i=0; i<numInputs; i++) {
+			for(int j=0; j<numOutputs; j++) {
+				//TODO set proper interval
+				weights[i][j] = random.nextFloat() * 2.0f - 1.0f;
+			}
+		}
+	}
 
 }
