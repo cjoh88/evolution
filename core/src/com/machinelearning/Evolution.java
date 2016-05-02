1265491143b911177c19084a1222bda27096f8c6
hampusadamsson <adamsson.hampus@gmail.com>
2016-05-02 13:49:17 +0200

    Pushing change: color reder.

--- core/src/com/machinelearning/Evolution.java
+++ core/src/com/machinelearning/Evolution.java
@@ -7,29 +7,29 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Evolution extends Game {
-	//SpriteBatch batch;
-	//Texture img;
-	
+	// SpriteBatch batch;
+	// Texture img;
+
 	EvolutionScreen mainScreen;
-	
+
 	@Override
-	public void create () {
-		//batch = new SpriteBatch();
-		//img = new Texture("badlogic.jpg");
+	public void create() {
+		// batch = new SpriteBatch();
+		// img = new Texture("badlogic.jpg");
 		mainScreen = new EvolutionScreen();
 		setScreen(mainScreen);
-		
+
 	}
 
 	@Override
-	public void render () {
+	public void render() {
 		super.render();
-		//Gdx.gl.glClearColor(0, 0, 0, 1);
-		//Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		//batch.begin();
-		//batch.draw(img, 0, 0);
-		//batch.end();
+		// Gdx.gl.glClearColor(0, 0, 0, 1);
+		// Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		// batch.begin();
+		// batch.draw(img, 0, 0);
+		// batch.end();
 	}
-	//HEJ
+	// HEJ
 }
